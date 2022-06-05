c1a2ce57b1bcd913dc4f3d5842f6b02a9e6cbc42
Cory Dela Cruz <corydcruz@gmail.com>
2022-06-04 17:14:41 -0700

    created readme

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 let startDate, endDate
-startDate = prompt("Start Date","YYYY-MM-DD")
-endDate = prompt("End Date")
+startDate = prompt("Start Date","2022-01-01")
+endDate = prompt("End Date", "2022-05-01")
 const url = `https://api.nasa.gov/planetary/apod?api_key=qqlQgdDqd0kHkZqkplnC0c3rylrZKAgYhsyXkH04&start_date=${startDate}&end_date=${endDate}`
-console.log(url)
+
+
 
 fetch(url)
     .then(res => {
         return res.json()
     })
     .then (res => {
-        console.log(res)
+        let arr= []
+        for(i=0; i < 10; i++){
+            arr.push(res[i])
+        }
+        console.log(arr)
     })
     .catch (err => {
         console.log(err)
