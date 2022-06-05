46ea87d6af65c134902972c9b94ff8bfeed1c11b
Cory Dela Cruz <corydcruz@gmail.com>
2022-06-04 18:03:24 -0700

    move to master

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-let startDate, endDate
-startDate = prompt("Start Date","2022-01-01")
-endDate = prompt("End Date", "2022-05-01")
+const startDate = "2022-01-01"
+const endDate = "2022-02-01"
+
 const url = `https://api.nasa.gov/planetary/apod?api_key=qqlQgdDqd0kHkZqkplnC0c3rylrZKAgYhsyXkH04&start_date=${startDate}&end_date=${endDate}`
 
+//DOM selectors
+let start = document.querySelector("#start")
+console.log(start)
+let end = document.querySelector("#end")
 
-
+function getData(e){
+    console.log(e)
 fetch(url)
     .then(res => {
         return res.json()
     })
     .then (res => {
+
         let arr= []
         for(i=0; i < 10; i++){
             arr.push(res[i])
@@ -19,4 +25,11 @@ fetch(url)
     .catch (err => {
         console.log(err)
     })
+}
+
+
+
+//event listeners
+start.addEventListener("input", getData)
+end.addEventListener("input", getData)
 
